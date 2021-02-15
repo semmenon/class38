fa7e2aa247798168483d8e66b50b6bb934c044e2
semmenon <72715354+semmenon@users.noreply.github.com>
2021-02-14 17:43:42 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var database, playerCount, gameState = 0
+var form, player, game
+var allPlayers
+var car1, car2, car3, car4
+var cars
+
+function setup(){
+    database = firebase.database ()
+    createCanvas(displayWidth-20,displayHeight-30);
+    game = new Game()
+    game.getState()
+    game.start()
+    
+}
+
+
+
+function draw(){
+    background("white")
+    if(playerCount == 4){
+        game.update(1)
+    }
+    if (gameState == 1){
+        game.play()
+    }
+}
